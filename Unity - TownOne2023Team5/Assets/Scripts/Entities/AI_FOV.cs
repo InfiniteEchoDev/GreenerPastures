ea077c23747dadbb2cba35b4beec97f238607650
KevinAlexV <Kev.vilanova@gmail.com>
2023-06-10 11:58:21 -0700

    Adds testing scenes and FOV scripts

---
+++ Unity - TownOne2023Team5/Assets/Scripts/Entities/AI_FOV.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_FOV : MonoBehaviour
+{
+    [Range(0, 360)]
+    public float viewAngle;
+    public float viewRadius;
+
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+
+    [HideInInspector]
+    public List<Transform> visibleTargets = new List<Transform>();
+
+    private void Start()
+    {
+        StartCoroutine("FindTargetsWithDelay", .2f);
+    }
+
+    IEnumerator FindTargetsWithDelay(float delay) 
+    {
+        while (true) 
+        {
+            yield return new WaitForSeconds (delay);
+            FindVisibleTargets();
+        
+        }
+    }
+
+    void FindVisibleTargets()
+    {
+        visibleTargets.Clear();
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+
+            //Target is within view angle
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2) 
+            {
+                float distToTarget = Vector3.Distance(transform.position, target.position);
+
+                //No obstacles in the way. we can see the target. Add it to the visible targets.
+                if (!Physics.Raycast(transform.position, dirToTarget, distToTarget, obstacleMask)) 
+                {
+                    visibleTargets.Add(target);
+                }
+
+            }
+        }
+    }
+
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool isAngleGlobal) 
+    {
+        if (!isAngleGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        
+        }
+
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad),0,Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
--- Unity - TownOne2023Team5/Assets/Scripts/Entities/WolfBehavior.cs
+++ Unity - TownOne2023Team5/Assets/Scripts/Entities/WolfBehavior.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class WolfBehavior : MonoBehaviour
 {
+    public float moveSpeed = 6.0f;
+
+    Rigidbody body;
+    Camera cam;
+    Vector3 velocity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody>();
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousepos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.transform.position.y));
+        transform.LookAt(mousepos + Vector3.up * transform.position.y);
+
+        velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * moveSpeed;
     }
+
+    private void FixedUpdate()
+    {
+        body.MovePosition(body.position + velocity * Time.fixedDeltaTime);
+    }
+
 }
