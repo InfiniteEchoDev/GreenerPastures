d849e773c1429bfe6b2b6a0441bdc8f82ea82e71
KevinAlexV <Kev.vilanova@gmail.com>
2023-06-10 14:59:52 -0700

    AI

---
+++ Unity - TownOne2023Team5/Assets/Scripts/Entities/Wolf/SheepPosition.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TheKiwiCoder;
+
+[System.Serializable]
+public class SheepPosition : ActionNode
+{
+    protected override void OnStart() 
+    {
+    }
+
+    protected override void OnStop() 
+    {
+    }
+
+    protected override State OnUpdate() 
+    {
+        AI_FOV agentSight = context.agent.GetComponent<AI_FOV>();
+
+        if (agentSight != null && agentSight.visibleTargets.Count != 0)
+        {
+            blackboard.moveToPosition = agentSight.visibleTargets[0].transform.position;
+            return State.Success;
+        }
+
+        return State.Failure;
+    }
+}
