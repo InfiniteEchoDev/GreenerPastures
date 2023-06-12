f92f68bca137f4ce51377de2f3a907bc5bdefd6b
KevinAlexV <Kev.vilanova@gmail.com>
2023-06-11 17:09:55 -0700

    Very basic wandering behavior reimplemented

--- Unity - TownOne2023Team5/Assets/Plugins/TheKiwiCoder/BehaviourTree/Scripts/Actions/RandomPosition.cs
+++ Unity - TownOne2023Team5/Assets/Plugins/TheKiwiCoder/BehaviourTree/Scripts/Actions/RandomPosition.cs
@@ -8,8 +8,8 @@ using UnityEngine.UIElements;
 [System.Serializable]
 public class RandomPosition : ActionNode 
 {
-    private Vector2 min = Vector2.one * -100;
-    private Vector2 max = Vector2.one * 100;
+    private Vector2 min = Vector2.one * -1;
+    private Vector2 max = Vector2.one * 1;
 
     protected override void OnStart() {
     }
@@ -18,12 +18,12 @@ public class RandomPosition : ActionNode
     }
 
     protected override State OnUpdate() {
-        Vector3 pos = new Vector3();
+        Vector3 pos = context.agent.transform.position;
 
-        pos.x = Random.Range(min.x, max.x);
-        pos.y = Random.Range(min.y, max.y);
-        //blackboard.moveToPosition = pos;
-        //Debug.Log("<color = yellow>[BT] Position is " + pos.x + " " + pos.y + "\nRange is " + min + " and " + max);
+        pos.x += Random.Range(min.x, max.x);
+        pos.y += Random.Range(min.y, max.y);
+        blackboard.moveToPosition = pos;
+        Debug.Log("<color=yellow>[BT] Position is " + pos.x + " " + pos.y + "\nRange is " + min + " and " + max + "</color>");
         return State.Success;
     }
 }
